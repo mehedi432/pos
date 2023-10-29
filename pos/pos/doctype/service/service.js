e34cbd9782a42371c2af07a0a99bf2adb23ba894
mehedi432 <aamehedi93@gmail.com>
2023-10-30 04:57:29 +0600

    Adding auto barcode for service mmodule

--- pos/pos/doctype/service/service.js
+++ pos/pos/doctype/service/service.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2023, Mehedi Abdullah and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Service", {
-// 	refresh(frm) {
+frappe.ui.form.on("Service", {
+	customer: function(frm) {
+        var barcodeValue = generateBarcode(frm.doc.customer);
+        frappe.msgprint(barcodeValue)
+        // Set the value of the barcode field
+        frm.set_value('barcode', barcodeValue);
+    }
+});
 
-// 	},
-// });
+function generateBarcode(customer) {
+    // Implement your barcode generation logic here.
+    // For this example, we'll use a simple concatenation.
+    return 'SERVICE-' + customer.replace(/\s+/g, '-').toUpperCase();
+}
